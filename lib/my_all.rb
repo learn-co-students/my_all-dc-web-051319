82bbd4072a563d41fea8ccf9e12e6fc937751f01
Emily Bolten <emilybolten@gmail.com>
2019-05-13 21:08:59 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  val = true
+  vals = []
+  while i < collection.length
+    i += 1
+    vals << yield(collection[i-1])
+  end
+
+  vals.each do |item|
+    if item == false
+      val =  false
+    end
+  end
+  return val
 
 end
